9337c2c83ff6f7f7f33ed3a358fc1bba39ae5f7b
putu nikayana <cungikawake@gmail.com>
2020-10-22 23:06:24 +0800

    fix

--- resources/views/member/profile.blade.php
+++ resources/views/member/profile.blade.php
@@ -19,7 +19,7 @@
                     @if(isset($member->logo) && $member->logo !='')
                         <img   src="{{ asset('storage/member/'.$member->logo) }}"  style="max-height:150px; border-radius:50%; border:2px solid #3EC7AF;">
                     @else
-                        <img   src="https://www.stma.org/wp-content/uploads/2017/10/no-image-icon.png"  style="max-height:150px; border-radius:50%; border:2px solid #3EC7AF;">
+                        <img   src="https://www.stma.org/wp-content/uploads/2017/10/no-image-icon.png"  style="height:100px; width:100px; border-radius:20%; border:2px solid #3EC7AF;">
                     @endif
                     <!-- <div class="change-user-thumb">
                         <form>
