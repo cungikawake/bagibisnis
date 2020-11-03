19d7699ce2d4e0a85190bcba1ecb3580ec27ca2a
putu nikayana <cungikawake@gmail.com>
2020-11-03 12:03:16 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,9 @@ Route::get('/shop/profile/{shop_name}', 'ShopController@shop_detail')->name('sho
 Route::get('/notif/{name}', 'NotifController@index')->name('notif');
 
 Auth::routes();
-Route::get('/register', 'Auth\RegisterController@getRegister')->name('register');
-Route::get('auth/{provider}', 'Auth\RegisterController@redirectToProvider');
-Route::get('auth/{provider}/callback', 'Auth\RegisterController@handleProviderCallback');
+Route::get('/register', 'Auth\LoginController@getRegister')->name('register');
+Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 /*
 |--------------------------------------------------------------------------
 |   ADMIN
